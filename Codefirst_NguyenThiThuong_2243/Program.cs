0493b524b07f2392bb45ac9d4873a9b95502a280
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-04-10 20:27:04 +0700

    Chỉ đường cho project đến sql bằng startup.cs

--- Codefirst_NguyenThiThuong_2243/Program.cs
+++ Codefirst_NguyenThiThuong_2243/Program.cs
@@ -9,8 +9,10 @@ using System.Threading.Tasks;
 
 namespace Codefirst_NguyenThiThuong_2243
 {
+    
     public class Program
     {
+        
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
--- Codefirst_NguyenThiThuong_2243/Startup.cs
+++ Codefirst_NguyenThiThuong_2243/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Codefirst_NguyenThiThuong_2243.Data;
 
 namespace Codefirst_NguyenThiThuong_2243
 {
@@ -24,6 +26,11 @@ namespace Codefirst_NguyenThiThuong_2243
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            //CODE Ở ĐÂY AddDbContext
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
