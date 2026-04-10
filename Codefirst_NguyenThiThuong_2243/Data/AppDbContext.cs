0aeecbef23ce9ff42529514339af9d2fb17dd43a
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-04-10 19:41:28 +0700

    Add project files.

---
+++ Codefirst_NguyenThiThuong_2243/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Codefirst_NguyenThiThuong_2243.Data
+{
+    public class AppDbContext
+    {
+    }
+}
---
+++ Codefirst_NguyenThiThuong_2243/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Codefirst_NguyenThiThuong_2243.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Requirded(ErroeMessage="Ten ko dc de trong")]
+        [StringLength(100)]
+        public string FullName { get; set; }
+        public int Age { set; get; }
+
+
+    }
+}
