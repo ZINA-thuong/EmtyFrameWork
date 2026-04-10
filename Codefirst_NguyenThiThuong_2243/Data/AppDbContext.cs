116161d7b27ea2d2718989c58978e162642f7b2b
Nguyen Thi Thuong <namthuong219@gmail.com>
2026-04-10 20:03:55 +0700

    B2 : khởi tạo DBCONTEXT

--- Codefirst_NguyenThiThuong_2243/Data/AppDbContext.cs
+++ Codefirst_NguyenThiThuong_2243/Data/AppDbContext.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Codefirst_NguyenThiThuong_2243.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Codefirst_NguyenThiThuong_2243.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students {get;set;}
     }
 }
